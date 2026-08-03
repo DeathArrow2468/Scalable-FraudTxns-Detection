aebb9f3096644a35928bf8137b7d0e2504327910
DeathArrow2468 <manav.hvm@gmail.com>
2026-08-04 01:21:47 +0530

    debugging

--- flink_feature_engineering/parser.py
+++ flink_feature_engineering/parser.py
@@ -3,15 +3,19 @@ from flink_feature_engineering.models import Transaction
 
 def parse_transaction(raw_json):
 
-    print("=" * 80)
-    print("TYPE:", type(raw_json))
-    print("RAW :", repr(raw_json))
-    print("=" * 80)
+    print("=" * 80, flush=True)
+    print("TYPE:", type(raw_json), flush=True)
+    print("RAW :", repr(raw_json), flush=True)
+    print("=" * 80, flush=True)
 
     if isinstance(raw_json, bytes):
         raw_json = raw_json.decode("utf-8")
 
-    data = json.loads(raw_json)
+    try:
+        data = json.loads(raw_json)
+    except Exception:
+        print("FAILED JSON:", repr(raw_json), flush=True)
+        raise
 
     return Transaction(
         event_number=int(data["event_number"]),
