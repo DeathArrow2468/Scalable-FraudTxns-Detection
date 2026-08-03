42bd33cc1c362158fd368b3cf5c39187a9dc715a
DeathArrow2468 <manav.hvm@gmail.com>
2026-08-03 18:53:04 +0530

    S3 upload

---
+++ train_dataset_maker/s3_uploader.py
@@ -0,0 +1,14 @@
+import boto3
+
+BUCKET_NAME = "fraud-txns-detection"
+LOCAL_FILE = "training_vectors.jsonl"
+S3_KEY = "train/training_vectors.jsonl"
+
+client = boto3.client("s3")
+
+def upload():
+    client.upload_file(LOCAL_FILE, BUCKET_NAME, S3_KEY)
+    print("S3 Upload success")
+
+if __name__ == "__main__":
+    upload()
