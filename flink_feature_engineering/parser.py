c76dd717b7b978f5d693b235d0ef09c1879e5e5f
DeathArrow2468 <manav.hvm@gmail.com>
2026-08-04 01:16:21 +0530

    debugging

--- flink_feature_engineering/parser.py
+++ flink_feature_engineering/parser.py
@@ -1,25 +1,29 @@
 import json
 from flink_feature_engineering.models import Transaction
 
-def parse_transaction(raw_json: str) -> Transaction:
+def parse_transaction(raw_json):
+
+    print("=" * 80)
+    print("TYPE:", type(raw_json))
+    print("RAW :", repr(raw_json))
+    print("=" * 80)
+
+    if isinstance(raw_json, bytes):
+        raw_json = raw_json.decode("utf-8")
+
     data = json.loads(raw_json)
 
     return Transaction(
         event_number=int(data["event_number"]),
         step=int(data["step"]),
         txn_id=data["txn_id"],
-
         type=data["type"],
         amount=float(data["amount"]),
-
         nameOrig=data["nameOrig"],
         nameDest=data["nameDest"],
-
         oldbalanceOrg=float(data["oldbalanceOrg"]),
         newbalanceOrig=float(data["newbalanceOrig"]),
-
         oldbalanceDest=float(data["oldbalanceDest"]),
         newbalanceDest=float(data["newbalanceDest"]),
-
         timestamp=int(data["timestamp"]),
     )
--- vector_sender_training.ps1
+++ vector_sender_training.ps1
@@ -1,38 +1,41 @@
 $eventNumber = 1
+
 Import-Csv .\paysim_shuffled.csv |
-Select-Object -First 1 | ForEach-Object{
-$txn = @{
+Select-Object -First 1 | ForEach-Object {
 
-    event_number = $eventNumber
-    step = [int]$_.step
-    txn_id = [guid]::NewGuid().ToString()
-   # user_id = $_.nameOrig #KAGGLE COLUMN: sender ID (for user velocity)
-    type = [string]$_.type
-    amount = [double]$_.amount
-    nameOrig = $_.nameOrig
-    nameDest = $_.nameDest
-    oldbalanceOrg = [double]$_.oldbalanceOrg
-    newbalanceOrig = [double]$_.newbalanceOrig
-    oldbalanceDest = [double]$_.oldbalanceDest
-    newbalanceDest = [double]$_.newbalanceDest
-    timestamp = [int64]((Get-Date).ToUniversalTime() - (Get-Date "1970-01-01")).TotalMilliseconds
-    isFraud = [int]$_.isFraud
-}
+    $txn = @{
+        event_number    = $eventNumber
+        step            = [int]$_.step
+        txn_id          = [guid]::NewGuid().ToString()
+        type            = [string]$_.type
+        amount          = [double]$_.amount
+        nameOrig        = $_.nameOrig
+        nameDest        = $_.nameDest
+        oldbalanceOrg   = [double]$_.oldbalanceOrg
+        newbalanceOrig  = [double]$_.newbalanceOrig
+        oldbalanceDest  = [double]$_.oldbalanceDest
+        newbalanceDest  = [double]$_.newbalanceDest
+        timestamp       = [int64]((Get-Date).ToUniversalTime() - (Get-Date "1970-01-01")).TotalMilliseconds
+        isFraud         = [int]$_.isFraud
+    }
 
-$jsonTxn = $txn | ConvertTo-Json -Compress
+    $jsonTxn = [string]($txn | ConvertTo-Json -Compress)
 
-if ([string]::IsNullOrWhiteSpace($jsonTxn)){
-    Write-Error "Invalid JSON generated: $jsonTxn"
-    return
-}
+    Write-Host "TYPE:"
+    $jsonTxn.GetType().FullName
 
-Write-Host $jsonTxn
+    Write-Host "`nJSON:"
+    Write-Host $jsonTxn
 
-aws kinesis put-record --stream-name fraud-txns --partition-key $txn.nameOrig --data $jsonTxn --cli-binary-format raw-in-base64-out
+    aws kinesis put-record `
+        --stream-name fraud-txns `
+        --partition-key $txn.nameOrig `
+        --data "$jsonTxn" `
+        --cli-binary-format raw-in-base64-out
 
-Write-Host "Sending Event #$eventNumber : $($txn.nameOrig) -> $($txn.nameDest)"
+    Write-Host "`nSent Event #$eventNumber"
 
-$eventNumber++
+    $eventNumber++
 
-Start-Sleep -Milliseconds 20 # x txns/sec
+    Start-Sleep -Milliseconds 20
 }
