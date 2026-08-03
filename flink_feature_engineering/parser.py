c041c0cddc3cbe23bba7f89e6ab912654a4ccf08
DeathArrow2468 <manav.hvm@gmail.com>
2026-08-04 01:29:39 +0530

    debug kinesis parser

--- flink_feature_engineering/parser.py
+++ flink_feature_engineering/parser.py
@@ -1,34 +1,30 @@
-import json
-import sys
 from flink_feature_engineering.models import Transaction
 
-def parse_transaction(raw_json):
+def parse_transaction(raw):
+    if isinstance(raw, bytes): raw = raw.decode("utf-8")
+    raw = raw.strip("{}")
+    data = {}
 
-    sys.stdout.write("=" * 80 + "\n")
-    sys.stdout.write(str(type(raw_json)) + "\n")
-    sys.stdout.write(repr(raw_json) + "\n")
-    sys.stdout.flush()
-
-    if isinstance(raw_json, bytes):
-        raw_json = raw_json.decode("utf-8")
-
-    try:
-        data = json.loads(raw_json)
-    except Exception:
-        print("FAILED JSON:", repr(raw_json), flush=True)
-        raise
+    for item in raw.split(","):
+        k, v = item.split(":", 1)
+        data[k.strip()] = v.strip()
 
     return Transaction(
         event_number=int(data["event_number"]),
         step=int(data["step"]),
         txn_id=data["txn_id"],
+
         type=data["type"],
         amount=float(data["amount"]),
+
         nameOrig=data["nameOrig"],
         nameDest=data["nameDest"],
+
         oldbalanceOrg=float(data["oldbalanceOrg"]),
         newbalanceOrig=float(data["newbalanceOrig"]),
+
         oldbalanceDest=float(data["oldbalanceDest"]),
         newbalanceDest=float(data["newbalanceDest"]),
+
         timestamp=int(data["timestamp"]),
     )
